734061e17f464c0c56153f5b0d1879847c3c3541
hakimscode <hhakimsetiawan@gmail.com>
2020-07-15 02:59:01 +0700

    add authentication

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import firebaseApp from './config/firebase';
+import Login from './components/Login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Product Management System
-        </p>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      user: {}
+    }
+
+    this.logoutHandle = this.logoutHandle.bind(this)
+  }
+
+  authListener = () => {
+    firebaseApp.auth().onAuthStateChanged((user)=>{
+      if(user){
+        this.setState({user})
+      }else{
+        this.setState({user: null})
+      }
+    })
+  }
+  
+  componentDidMount = () => {
+    this.authListener();
+  }
+
+  logoutHandle = (e) => {
+    e.preventDefault();
+    firebaseApp.auth().signOut();
+  }
+  
+  render(){
+    return (
+      <div className="App">
+        {
+          this.state.user 
+          ? (
+            <div>
+              <h1>You are Logged In</h1>
+              <button onClick={this.logoutHandle}>Logout</button>
+            </div>
+          )
+          : <Login/>}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import firebaseApp from '../config/firebase';
+
+class Login extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            email: "",
+            password: ""
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.signInHandle = this.signInHandle.bind(this);
+        this.signUpHandle = this.signUpHandle.bind(this);
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
+    signInHandle = (e) => {
+        e.preventDefault();
+        firebaseApp.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+        .then((res) => {
+            console.log(res);
+        })
+        .catch((err) => {
+            alert(err.message);
+        })
+    }
+
+    signUpHandle = (e) => {
+        e.preventDefault();
+        firebaseApp.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then((res) => {
+            console.log(res);
+        })
+        .catch((err) => {
+            alert(err.message);
+        })
+    }
+
+    render(){
+        return (
+          <div>
+              <form>
+                  <input
+                  type="text"
+                  name="email"
+                  onChange={this.handleChange}
+                  placeholder="Email"
+                  required/>
+                  
+                  <input
+                  type="password"
+                  name="password"
+                  onChange={this.handleChange}
+                  placeholder="Password"
+                  required/>
+
+                  <button onClick={this.signInHandle}>Sign In</button>
+                  <button onClick={this.signUpHandle}>Sign Up</button>
+              </form>
+          </div>
+        );
+    }
+}
+
+export default Login;
